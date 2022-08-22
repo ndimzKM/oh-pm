27491c141ff25d5c8b6f4530957ddeda1a20bff3
Alieu Ndimbalane <alieundimbalane@gmail.com>
2022-08-22 02:16:12 +0000

    prettier file formating

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
-import { resolve } from "path"
-import { createHash } from "crypto";
-import express, { Application, Request, Response } from "express"
-import PouchDB from "pouchdb";
-import { Level } from "level"
-import PouchServer from "express-pouchdb"
-import cors from "cors";
-import axios from "axios";
+import { resolve } from 'path';
+import { createHash } from 'crypto';
+import express, { Application, Request, Response } from 'express';
+import PouchDB from 'pouchdb';
+import { Level } from 'level';
+import PouchServer from 'express-pouchdb';
+import cors from 'cors';
+import axios from 'axios';
 
-import { CorsValues } from "./utils/configs";
-import argparser from "./utils/args"
+import { CorsValues } from './utils/configs';
+import argparser from './utils/args';
 const argv = argparser(process.argv);
 
-const Store = new Level(resolve(argv.dir, 'filehost'), { valueEncoding: 'binary' });
-const app:Application = express();
+const Store = new Level(resolve(argv.dir, 'filehost'), {
+    valueEncoding: 'binary',
+});
+const app: Application = express();
 const pouchapp = PouchServer(PouchDB);
 
-pouchapp.use(cors(CorsValues))
+pouchapp.use(cors(CorsValues));
 
-pouchapp.listen(argv.db_port, () => console.log(`PouchServer running`))
+pouchapp.listen(argv.db_port, () => console.log(`PouchServer running`));
 
 const REMOTE_SKIM = argv.skim;
 const LOCAL_SKIM = 'opm_new';
@@ -25,84 +27,111 @@ const LOCAL_SKIM = 'opm_new';
 const localSkim = new PouchDB(LOCAL_SKIM);
 const remoteSkim = new PouchDB(REMOTE_SKIM);
 
-app.get('/:name', (req: Request,res: Response) => {
-    let packageName:string = req.params.name;
-    getPackage(packageName).then((pkg:any) => {
-        const versions = Object.keys(pkg.versions);
-        versions.forEach(version => {
-            pkg.versions[version].dist.tarball = `${argv['url']}/tarballs/${pkg.name}/${version}.tgz`
-            pkg.versions[version].dist.info = `${argv['url']}/${pkg.name}/${version}`;
+app.get('/:name', (req: Request, res: Response) => {
+    let packageName: string = req.params.name;
+    getPackage(packageName)
+        .then((pkg: any) => {
+            const versions = Object.keys(pkg.versions);
+            versions.forEach(version => {
+                pkg.versions[
+                    version
+                ].dist.tarball = `${argv['url']}/tarballs/${pkg.name}/${version}.tgz`;
+                pkg.versions[
+                    version
+                ].dist.info = `${argv['url']}/${pkg.name}/${version}`;
+            });
+
+            res.send(pkg);
         })
+        .catch(err => {
+            res.status(500).send(err);
+        });
+});
 
-        res.send(pkg);
-    }).catch(err => {
-        res.status(500).send(err)
-    })
-    
-})
-
-app.get('/tarballs/:name/:version.tgz', (req: Request,res: Response) => {
-    let packageName:string = req.params.name;
+app.get('/tarballs/:name/:version.tgz', (req: Request, res: Response) => {
+    let packageName: string = req.params.name;
     let version = req.params.version;
     let id = `${packageName}-${version}`;
 
-
-    getPackage(packageName).then((pkg:any) => {
-        Store.get(id, {keyEncoding:'buf', valueEncoding: 'binary' }, (err, buf: any) => {
-            if(!err) {
-                let hash = createHash('sha1');
-                hash.update(buf);
-                if(pkg.versions[version].dist.shasum !== hash.digest('hex')) {
-                    res.status(500).send({
-                        error: 'hashes do not match'
-                    })
-                }else {
-                    res.setHeader('Content-Type', 'application/octet-stream')
-                    res.setHeader('Content-Length', buf.length);
-                    res.send(buf);
+    getPackage(packageName)
+        .then((pkg: any) => {
+            Store.get(
+                id,
+                { keyEncoding: 'buf', valueEncoding: 'binary' },
+                (err, buf: any) => {
+                    if (!err) {
+                        let hash = createHash('sha1');
+                        hash.update(buf);
+                        if (
+                            pkg.versions[version].dist.shasum !==
+                            hash.digest('hex')
+                        ) {
+                            res.status(500).send({
+                                error: 'hashes do not match',
+                            });
+                        } else {
+                            res.setHeader(
+                                'Content-Type',
+                                'application/octet-stream'
+                            );
+                            res.setHeader('Content-Length', buf.length);
+                            res.send(buf);
+                        }
+                    } else {
+                        let tgzLocation = pkg.versions[version].dist.tarball;
+                        axios
+                            .get(tgzLocation)
+                            .then(content => {
+                                res.setHeader(
+                                    'Content-Type',
+                                    'application/octet-stream'
+                                );
+                                res.setHeader(
+                                    'Content-Length',
+                                    content.data.length
+                                );
+                                res.send(content);
+                            })
+                            .catch(err => {
+                                res.status(500).send(err);
+                            });
+                    }
                 }
-            }else {
-                let tgzLocation = pkg.versions[version].dist.tarball;
-                axios.get(tgzLocation)
-                    .then(content => {
-                        res.setHeader('Content-Type', 'application/octet-stream')
-                        res.setHeader('Content-Length', content.data.length);
-                        res.send(content);    
-                    })
-                    .catch(err => {
-                        res.status(500).send(err)
-                    })
-            }
-        }) 
+            );
 
-        pkg.versions[version].downloads ? pkg.versions[version].downloads += 1 : pkg.versions[version].downloads = 1;
-        localSkim.put(pkg);
-    })
-
-    .catch(() => {
-        res.status(500).send({
-            error: 'Unexpected error'
+            pkg.versions[version].downloads
+                ? (pkg.versions[version].downloads += 1)
+                : (pkg.versions[version].downloads = 1);
+            localSkim.put(pkg);
         })
-    })
-})
+
+        .catch(() => {
+            res.status(500).send({
+                error: 'Unexpected error',
+            });
+        });
+});
 // app.get('/tarballs/:user/:package/:version.tgz', (req: Request,res:Response) => {})
 
 app.listen(argv.port, () => {
-    console.log(`Server started on port ${argv.port}`)
-})
+    console.log(`Server started on port ${argv.port}`);
+});
 
 function getPackage(name: string) {
-    return localSkim.get(name)
+    return localSkim
+        .get(name)
         .catch(() => {
-            return remoteSkim.get(name)
+            return remoteSkim
+                .get(name)
                 .then((pkg: any) => {
-                    delete pkg['_rev']
-                    return localSkim.post(pkg)
+                    delete pkg['_rev'];
+                    return localSkim.post(pkg);
                 })
                 .then(() => {
                     return localSkim.get(name);
-                })
-            }).catch(err => {
-                console.error('You are offline and package not found locally')
-            })
+                });
+        })
+        .catch(err => {
+            console.error('You are offline and package not found locally');
+        });
 }
--- src/utils/args.ts
+++ src/utils/args.ts
@@ -1,58 +1,62 @@
-import { supportedArgs, args, ArgList, ArgKey } from "./configs";
+import { supportedArgs, args, ArgList, ArgKey } from './configs';
 
 export default function argparser(arglist: ArgList) {
-    arglist = arglist.slice(2)
+    arglist = arglist.slice(2);
 
-    if(arglist[0] == '-h' || arglist[0] == '--help')
-        displayHelp()
+    if (arglist[0] == '-h' || arglist[0] == '--help') displayHelp();
 
-    if(arglist.length % 2 !== 0)
-        error('Error: uneven number of arguments');
+    if (arglist.length % 2 !== 0) error('Error: uneven number of arguments');
 
-    for(let i = 0; i < arglist.length; i++) {
-        let firstChars = arglist[i].substring(0,2);
-        if(firstChars.includes('-')){
+    for (let i = 0; i < arglist.length; i++) {
+        let firstChars = arglist[i].substring(0, 2);
+        if (firstChars.includes('-')) {
             try {
-                if(arglist[i+1].substring(0,1).includes('-'))
-                    error(`Error: value of ${arglist[i]} cannot contain that symbol`)
+                if (arglist[i + 1].substring(0, 1).includes('-'))
+                    error(
+                        `Error: value of ${arglist[i]} cannot contain that symbol`
+                    );
 
-                if(!supportedArgs.includes(arglist[i]))
-                    error(`Error: ${arglist[i]} is not supported as an argument`)
+                if (!supportedArgs.includes(arglist[i]))
+                    error(
+                        `Error: ${arglist[i]} is not supported as an argument`
+                    );
 
                 let currentArg: ArgKey = arglist[i];
-                args[currentArg] = arglist[i+1];
-            }catch(error) {
+                args[currentArg] = arglist[i + 1];
+            } catch (error) {
                 console.error(error);
             }
         }
-    } 
-    if(arglist.length > 1 && arglist.includes('-v') || arglist.includes('--version'))
-        error('Error: you cannot get the version this way')
+    }
+    if (
+        (arglist.length > 1 && arglist.includes('-v')) ||
+        arglist.includes('--version')
+    )
+        error('Error: you cannot get the version this way');
 
     return {
-        'port': args["-p"] as number,
-        'db_port': args["-pp"] as number,
-        'skim': args["-s"] as string,
-        'url': args["-u"] as string,
-        'dir': args["-d"] as string
-    }
+        port: args['-p'] as number,
+        db_port: args['-pp'] as number,
+        skim: args['-s'] as string,
+        url: args['-u'] as string,
+        dir: args['-d'] as string,
+    };
 }
 
 function error(msg: string) {
-    console.error(msg)
+    console.error(msg);
     process.exit(-1);
 }
 
 function displayHelp() {
+    console.log('Usage: opm [arguments]');
+    console.log('\nOptions:');
+    console.log('    -v      get version number of opm');
+    console.log('    -h      display this help message');
+    console.log('    -p      port for your local registry');
+    console.log('    -pp     port for your pouch db server');
+    console.log('    -u      url to use for your local registry');
+    console.log('    -d      directory to store pouchdb data');
 
-    console.log("Usage: opm [arguments]");
-    console.log('\nOptions:')
-    console.log('    -v      get version number of opm')
-    console.log('    -h      display this help message')
-    console.log('    -p      port for your local registry')
-    console.log('    -pp     port for your pouch db server')
-    console.log('    -u      url to use for your local registry')
-    console.log('    -d      directory to store pouchdb data')
-
-    process.exit(0)
+    process.exit(0);
 }
